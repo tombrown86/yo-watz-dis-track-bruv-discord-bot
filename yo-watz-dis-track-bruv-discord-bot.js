3d4b271683cf10a09fe1e41e96c5363bf49ff7f0
Tom Brown <tombrown86@gmail.com>
2020-09-14 17:24:28 +0100

    implemented it

--- yo-watz-dis-track-bruv-discord-bot.js
+++ yo-watz-dis-track-bruv-discord-bot.js
@@ -1,54 +1,103 @@
 const Discord = require('discord.js');
 const client = new Discord.Client();
 const fs = require('fs');
-const { spawn } = require('child_process');
+const { exec } = require('child_process');
+const { Readable } = require('stream');
 
 
-const RADIO_USERNAME = 'radio nv';
-const SNIPPET_LENGTH_SECS = 10;
-const RECOGNIZER_CMD = `python3 /home/tom/repos/freezam/interface.py -vb identify --pathfile="{0}" --type=1`;
-const RECOGNIZER_CMD2 = `python3 /home/tom/repos/freezam/interface.py -vb identify --pathfile="{0}" --type=2`;
+//const RADIO_USERNAME = 'radio nv';
+const RADIO_USERNAME = 'tombrown86';
+const SNIPPET_LENGTH_SECS = 20;
+const CONVERT_CMD = `ffmpeg -f s16le -ar 48k -ac 2 -i %pcmpath% %wavpath%`;
+const RECOGNIZER_CMD = `python3 /home/tom/repos/freezam/interface.py identify --pathfile="%path%" --type=2`; // 2 is better apparently
+const RECOGNIZER_CMD2 = `python3 /home/tom/repos/freezam/interface.py identify --pathfile="%path%" --type=1`;
+
+
+class Silence extends Readable {
+    _read() {
+        this.push(Buffer.from([0xF8, 0xFF, 0xFE]));
+    }
+}
 
 client.on('message', async message => {
 	// Join the same voice channel of the author of the message
 	
-	if(['!track'].indexOf(receivedMessage.content) > -1)  {
-		receivedMessage.channel.send('yo watz dis track bruv?? listening...');
-		if (message.member.voice.channel) {
-            const connection = await message.member.voice.channel.join();
-            const user = client.users.find("username", RADIO_USERNAME);
-            const audioStream = connection.receiver.createStream(user, {mode: 'pcm', end: "manual"});
-            const path = '/tmp/watzdistrack_audio_'+(new Date().getTime());
-            audioStream.pipe(fs.createWriteStream(path));
+	if(['!track', '!trackdebug'].indexOf(message.content) > -1)  {
+        try {
+            message.channel.send('listening...');
+            if (message.member.voice.channel) {
+                const connection = await message.member.voice.channel.join();
+                connection.play(new Silence(), { type: 'opus' });// start sending silence otherwise we can't receive!
 
-            setTimeout(function() {
-                exec_recognize(RECOGNIZER_CMD, path, receivedMessage.channel.send, function() {exec_recognize(RECOGNIZER_CMD2, path, receivedMessage.channel.send)});
-            }, SNIPPET_LENGTH_SECS * 1000 /*milliseconds*/)
-		} else {
-            receivedMessage.channel.send('ask me in voice channel init plz');
+                const user = client.users.cache.find(u => u.username === RADIO_USERNAME);
+
+
+                const audioStream = connection.receiver.createStream(user, {mode: 'pcm', end: "manual"});
+                const path = '/tmp/watzdistrack_audio_'+(new Date().getTime());
+                audioStream.pipe(fs.createWriteStream(path));
+
+                setTimeout(function() {
+                    message.member.voice.channel.leave();
+                    message.channel.send('let me think...');
+                    
+
+                    exec(CONVERT_CMD.replace('%pcmpath%', path).replace('%wavpath%', path + '.wav'), (error, stdout, stderr) => {
+                        console.log(stdout);
+                    }).on('close', function () {
+                        exec_recognize(RECOGNIZER_CMD, path + '.wav', msg => {message.channel.send(msg)},
+                            // onsuccess
+                            path => del_recording, 
+                            // onfail
+                            function() {exec_recognize(RECOGNIZER_CMD2, path + '.wav',  msg => {message.channel.send(msg)}, path => del_recording)
+                        });
+                    });
+                }, SNIPPET_LENGTH_SECS * 1000)
+            } else {
+                message.channel.send('ask in a voice channel init plz');
+            }
+        } catch(err) {
+            console.log(err);
+            message.channel.send(message.content === '!trackdebug' ? 'oh dear:' + err  : "unable to listen to " + RADIO_USERNAME + ':-(');
         }
 	}
 });
 
-function exec_recognize(cmd_template, path, out, onfail) {
-    const cmd = String.format(cmd_template, path);
+function del_recording(path) {
+    try {
+        fs.unlinkSync(path);
+        fs.unlinkSync(path + '.wav');
+    } catch(err) {
+        console.error(err)
+    }
+}
+
+function exec_recognize(cmd_template, path, out, onsuccess, onfail) {
+    const cmd = cmd_template.replace('%path%', path);
+    out(cmd);
+
     exec(cmd, (error, stdout, stderr) => {
         if (error) {
+            console.log(`oh dear: ${error.message}`);
             out(`oh dear: ${error.message}`);
             onfail && onfail();
             return;
         }
         if (stderr) {
+            console.log(`oh dear: ${stderr}`);
             out(`oh dear: ${stderr}`);
             onfail && onfail();
             return;
         }
         
         if(stdout.indexOf('The best match is' > -1)) {
-            out(stdout.split('\n')[0]);
+            console.log(stdout.split("\n"));
+            out(stdout.split("\n")[0]);
+            onsuccess && onsuccess();
         } else {
             onfail && onfail();
         }
+
+
     });
 }
 
